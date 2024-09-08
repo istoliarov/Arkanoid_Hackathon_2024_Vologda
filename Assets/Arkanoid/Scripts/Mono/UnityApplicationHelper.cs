36bb10f6ce711ca8f692d06b84e756deba42d13a
Igor Stolyarov <balalaikaputinka@outlook.com>
2024-09-08 22:58:38 +0300

    Windows build

---
+++ Assets/Arkanoid/Scripts/Mono/UnityApplicationHelper.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Arkanoid.Scripts.Mono
+{
+    public class UnityApplicationHelper : MonoBehaviour
+    {
+        public void Exit()
+        {
+            UnityEngine.Application.Quit();
+        }
+    }
+}
